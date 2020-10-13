301829560b3b2027bef7c2547662fb14fbb7a908
azeezdon <azeezdonself@gmail.com>
2020-10-14 00:47:50 +0100

    first commit

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -10,14 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
-from blog import MyWSGIApp
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
-application = MyWSGIApp()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')

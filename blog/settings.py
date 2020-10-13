0a39b7b50b30bd4bcb6dc733f38c83a6ec996a78
azeezdon <azeezdonself@gmail.com>
2020-10-13 22:37:06 +0100

    first commit

--- blog/settings.py
+++ blog/settings.py
@@ -157,7 +157,7 @@ STATIC_URL = 'app/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'app/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Extra places for collectstatic to find static files.

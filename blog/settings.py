225e14a13d77dcd7dd75a4b4f39459bcae523458
azeezdon <azeezdonself@gmail.com>
2020-10-13 22:48:52 +0100

    first commit

--- blog/settings.py
+++ blog/settings.py
@@ -16,7 +16,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -151,14 +150,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'app/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Extra places for collectstatic to find static files.
 
